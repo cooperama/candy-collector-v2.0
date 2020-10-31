bd6c2af03c5d3f6c13c29000f7546d8adb32b25e
cooperama <coope133@gmail.com>
2020-10-31 14:21:11 -0700

    Adds login, signup, and logout functionality

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.urls import reverse #
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+class Store(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    store_name = models.CharField(max_length=50)
+    location = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.store_name
--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,14 @@ urlpatterns = [
     # Static
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    # Candy
+    # path('candy/', views.candy_index, name='candy_index'),
+    # path('candy/<int:candy_id>/', views.candy_detail, name='candy_detail'),
+    # path('stores/<int:store_id>/candy/new/', views.add_candy, name='add_candy'),
+    # Seller(user)
+    path('accounts/signup/', views.signup, name='signup'),
+    # Stores
+    # path('stores/', views.stores_index, name='stores_index'),
+    path('userstores/', views.user_stores, name='user_stores'),
+    # path('stores/<int:store_id>/', views.store_detail, name='store_detail'),
 ]
